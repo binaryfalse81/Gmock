f259bc6e1b68a37ff22adb42d1b832a1b7d798a7
JJY <binaryfalse81@gmail.com>
2024-07-04 12:00:28 +0900

    code 추가

--- Project1/cal.cpp
+++ Project1/cal.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <stdexcept>
 using namespace std;
 
 class Cal
@@ -7,5 +9,25 @@ public:
     int getSum(int a, int b)
     {
         return a + b;
+    }
+    vector<int> getResult(const vector<int>& input)
+    {
+        vector<int> output;
+        for (int i = 0; i < input.size(); i++)
+        {
+            int num = input[i];
+            if (num >= 10 || num == 4)
+            {
+                throw invalid_argument("INV");
+            }
+
+            num++;
+            if (num == 4) num++;
+            if (num == 10) num = 0;
+
+            output.push_back(num);
+        }
+
+        return output;
     }
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -2,9 +2,53 @@
 #include "gmock/gmock.h"
 #include "../Project1/cal.cpp"
 
+using namespace testing;
+
 TEST(TestCaseName, TestName)
 {
+    EXPECT_THAT(1, Eq(1));
+
+    vector<int> arr = { 1, 2, 3 };
+    EXPECT_THAT(arr, Contains(3));
+
+    vector<int> arr1;
+    EXPECT_THAT(arr1, IsEmpty());
+}
+
+TEST(TestCaseName, TestName2)
+{
+    vector<int> input = { 1, 2, 3, 2 };
+    vector<int> expect = { 2, 3, 5, 3 };
     Cal c;
-    int ret = c.getSum(1, 1);
-    EXPECT_EQ(ret, 2);
+    vector<int> actual = c.getResult(input);
+
+    EXPECT_EQ(expect, actual);
+    EXPECT_THAT(expect, ContainerEq(actual));
+}
+
+TEST(TestCaseName, TestName3)
+{
+    vector<int> input = { 9, 9, 3, 3 };
+    vector<int> expect = { 0, 0, 5, 5 };
+    Cal c;
+    vector<int> actual = c.getResult(input);
+
+    EXPECT_EQ(expect, actual);
+    EXPECT_THAT(expect, ContainerEq(actual));
+}
+
+TEST(TestCaseName, TestName4)
+{
+    vector<int> input = { 10, 10, 10, 10 };
+    Cal c;
+
+    try
+    {
+        vector<int> actual = c.getResult(input);
+    }
+    catch (invalid_argument& e)
+    {
+        string temp = e.what();
+        EXPECT_THAT(string("INV"), ContainerEq(temp));
+    }
 }
